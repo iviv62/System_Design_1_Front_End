64b4ad29beb6550efa6b79f6f818406e1a6b824e
Ivelin Ivanov <49588161+iviv62@users.noreply.github.com>
2026-05-19 12:50:19 +0200

    fix(landing-orbits): remove unused unsafeCSS import

--- frontend/src/components/nebula/landing-orbits.ts
+++ frontend/src/components/nebula/landing-orbits.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css, unsafeCSS } from "lit";
+import { LitElement, html, css } from "lit";
 import { customElement } from "lit/decorators.js";
 import { ORBIT_USERS } from "./landing-orbit-data";
 import { userIcon } from "./landing-icons";
