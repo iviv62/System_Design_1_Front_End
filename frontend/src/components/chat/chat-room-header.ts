0c3e471ac751ca23282aabf28ff10311153f6266
iviv62 <ivcho2213@gmail.com>
2026-05-19 12:33:01 +0200

    fix: remove members icon

--- frontend/src/components/chat/chat-room-header.ts
+++ frontend/src/components/chat/chat-room-header.ts
@@ -39,9 +39,6 @@ export class ChatRoomHeader extends LitElement {
     this.dispatchEvent(new CustomEvent(event, { bubbles: true, composed: true }));
   }
 
-  private handleToggleMembersClick() {
-    this.dispatchEvent(new CustomEvent("toggle-members", { bubbles: true, composed: true }));
-  }
 
   render() {
     return html`
@@ -73,17 +70,7 @@ export class ChatRoomHeader extends LitElement {
             >
               ${iconVideo}
             </button>
-            <button
-              class="chat-room__header-action ${this.showMembers
-                ? "chat-room__header-action--active"
-                : ""}"
-              type="button"
-              title="Toggle members"
-              aria-label="Toggle members panel"
-              @click=${this.handleToggleMembersClick}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M23 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>
-            </button>
+
           </div>
         </div>
       </header>
